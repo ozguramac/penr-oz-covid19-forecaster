191881a80aacd548820876c928cad27ac9a514be
Özgür <ozgur.v.amac@derinworksllc.com>
2020-04-08 23:10:42 -0400

    Init

---
+++ README.md
@@ -0,0 +1,50 @@
+# Penr-oz Covid-19 Forecaster
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE](https://github.com/CSSEGISandData/COVID-19)
+* [Docker Compose Overview](https://docs.docker.com/compose/overview/) 
+* [Python - Pandas](https://pandas.pydata.org/)
+* [Python - Matplotlib](https://matplotlib.org/)
+* [Data Science - ARIMA model](https://en.wikipedia.org/wiki/Autoregressive_integrated_moving_average)
+
+### Additional Links
+These additional references should also help you:
+
+* [Flask](https://flask.palletsprojects.com/en/1.1.x/)
+* [Jinja](https://jinja.palletsprojects.com/en/2.11.x/)
+* [Python - Time Series Forecasting](https://machinelearningmastery.com/time-series-forecasting-methods-in-python-cheat-sheet/)
+
+## Download and Installation
+* [Fork, Clone, or Download on GitHub](https://github.com/ozguramac/penr-oz-covid19-forecaster)
+
+### Run
+```
+$ docker-compose up penr-oz-covid19-forecaster
+```
+
+Launch your favorite browser and goto [localhost:5000](http://localhost:5000)
+
+### Stop
+```
+$ docker-compose stop
+```
+
+### Clean
+```
+$ docker-compose down
+```
+
+### Check/Health
+```
+docker-compose ps
+```
+
+### Who do I talk to? ###
+* Repo owner or [admin](mailto:info@derinworksllc.com) 
+
+## Copyright and License
+&copy; Copyright 2020 [Derin Works LLC](http://www.derinworksllc.com)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ src/app.py
@@ -0,0 +1,69 @@
+import base64
+import io
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from cachetools import cached, TTLCache
+
+from statsmodels.tsa.arima_model import ARIMA
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+categories = ['confirmed_US', 'deaths_US', 'deaths_global', 'recovered_US']
+
+
+@app.route('/')
+def root():
+    return render_template("index.html", categories=categories, category=categories[0], key='Massachusetts, US',
+                           num_of_days=3, p=0, q=1, d=1)
+
+
+@app.route('/forecast', methods=['POST'])
+def response():
+    category = request.form.get("category")
+    key = request.form.get("key")
+    num_of_days = int(request.form.get("num_of_days", "3"))
+    p = int(request.form.get("p", "0"))
+    q = int(request.form.get("q", "1"))
+    d = int(request.form.get("d", "1"))
+
+    df = read_csv(category)
+
+    app.logger.info('Aggregating by ' + key)
+    dr = df.loc[[k for k in df.index if key in k], df.columns[11:]].sum()
+
+    app.logger.info('Forecasting {} day(s) out using ARIMA with order=({},{},{})'.format(num_of_days, p, q, d))
+    model = ARIMA(dr.values, order=(p, q, d))
+    model_fit = model.fit(disp=False)
+    yhat = model_fit.predict(len(dr.values), len(dr.values) + num_of_days, typ='levels')
+
+    app.logger.info('Preparing plot...')
+    plt.figure(figsize=(10, 10), dpi=100)
+    fig, (ax1, ax2) = plt.subplots(2)
+    ax1.plot(dr.index[-num_of_days:], dr.values[-num_of_days:], color='tab:red')
+    ax1.set(title='COVID-19 ' + category + ' ' + key, xlabel='dates', ylabel='num of ' + category)
+    ax2.plot(range(0, len(yhat)), yhat, color='tab:blue')
+    ax2.set(xlabel='days', ylabel=category + ' forecast')
+
+    app.logger.info('Converting plot to png...')
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_data = base64.b64encode(img.getvalue()).decode()
+
+    return render_template("index.html", categories=categories, category=category, key=key,
+                           num_of_days=num_of_days, p=p, q=q, d=d, plot=plot_data)
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=3*60*60))
+def read_csv(cat: str) -> pd.DataFrame:
+    base_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/'
+    url = base_url + '/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_' + cat + '.csv'
+    idx = 'Combined_Key' if cat.endswith('_US') else 'Country/Region'
+    app.logger.info('Reading CSV from ' + url + " and indexing on " + idx)
+    return pd.read_csv(url, index_col=idx)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
